a32e9143acc86715e474bf4df1be943ae39c12bf
Sairam R Sureshbabu <165698094+saizzz-dmg@users.noreply.github.com>
2025-04-05 16:13:42 +0530

    Manual mode feature included

--- mpu6050_fin_ino.ino
+++ mpu6050_fin_ino.ino
@@ -7,15 +7,19 @@ int left_forward  = 9;
 int left_reverse = 10;
 int right_forward = 11;
 int right_reverse = 12;
-
 int time = 0;
 char dir = NULL;
 char prev_dir = NULL;
+int manualPin = 4;
+int leftForward = 3;  
+int leftBackward = 2;
+int rightForward = 7;
+int rightBackward = 6;
+bool newState = false;
 
-void setup() 
-{
+
+void setup() {
   Serial.begin(115200);
-
   Serial.println("Initialize MPU6050");
 
   while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
@@ -27,10 +31,51 @@ void setup()
   pinMode(left_reverse , OUTPUT);
   pinMode(right_forward , OUTPUT);
   pinMode(right_reverse , OUTPUT);
+  pinMode(manualPin, INPUT_PULLUP);  
+  pinMode(leftForward , INPUT);
+  pinMode(leftBackward , INPUT);
+  pinMode(rightForward , INPUT);
+  pinMode(rightBackward , INPUT);
 }
 
-void loop()
-{
+void loop() {
+  if (digitalRead(manualPin) == LOW) {  
+    newState = true;
+
+    Serial.println("manual");
+    if (digitalRead(leftForward) == HIGH){
+      Serial.println("left forward");
+      digitalWrite(left_forward , HIGH);
+    }
+    else if (digitalRead(leftBackward) == HIGH){
+      Serial.println("left Backward");
+      digitalWrite(left_reverse , HIGH);
+      
+    }
+    else if (digitalRead(rightForward) == HIGH){
+      Serial.println("right forward");
+      digitalWrite(right_forward, HIGH);
+    }
+    else if (digitalRead(rightBackward) == HIGH){
+      Serial.println("right backward");
+      digitalWrite(right_reverse , HIGH);
+    }
+    else{
+      digitalWrite(right_forward, LOW);
+      digitalWrite(left_reverse , LOW);
+      digitalWrite(left_forward , LOW);
+      digitalWrite(right_reverse , LOW);
+
+    }
+  }
+  else {  
+    if (newState == true){
+      int time = 0;
+      char dir = NULL;
+      char prev_dir = NULL;
+      bool newState = false;
+    }
+
   Vector normAccel = mpu.readNormalizeAccel();
   int tilt = (atan2(normAccel.YAxis, normAccel.ZAxis)*180.0)/M_PI;
   Serial.print(" Tilt = ");
@@ -41,7 +86,7 @@ void loop()
     if (dir && time){
       Serial.print("time balancing");
       digitalWrite(right_reverse , HIGH);
-      delay(time);
+      delay(time+1000);
       digitalWrite(right_reverse , LOW);
       prev_dir = NULL;
       dir = NULL;
@@ -58,7 +103,7 @@ void loop()
     if (dir && time){
       Serial.print("time balancing");
       digitalWrite(left_reverse , HIGH);
-      delay(time);
+      delay(time+1000);
       digitalWrite(left_reverse , LOW);
       prev_dir = NULL;
       dir = NULL;
@@ -83,14 +128,7 @@ void loop()
   delay(100);
 
 }
-    
-
-
-
-
-
-
-
+  }
 
 
 
